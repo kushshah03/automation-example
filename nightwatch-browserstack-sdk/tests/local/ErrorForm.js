f7e8ca7002d78109cc962b123eea6e951cbc11a7
Kush Shah <kush@browserstack.com>
2023-07-13 15:26:41 +0530

    added new local tests and unit tests

---
+++ nightwatch-browserstack-sdk/tests/local/ErrorForm.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'Field Required' : function (browser) {
+      browser
+        .url('http://localhost:3000')
+        .getTitle(function(title) {
+          this.assert.ok(title.includes('Demo App'));
+        })
+        .click("input[value='Submit']")
+        .waitForElementVisible('#error_message');
+      browser.expect.element('#error_message').text.to.equal('All fields are required');
+      browser.percySnapshot('Error Form');
+      browser.end();
+    },
+  };
+  
